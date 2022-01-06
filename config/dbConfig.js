c333b47d8cea04ddfa5703d861925e6bcb9e2e16
Nmuta Jones <nmuta1@gmail.com>
2022-01-06 08:06:11 -0700

    Add db config

---
+++ config/dbConfig.js
@@ -0,0 +1,8 @@
+// store the configuration secrets of the PG DB here
+module.exports = {
+    HOST: 'database-2.c19m1d29riqs.us-east-1.rds.amazonaws.com', // your endpoint
+    USER: 'postgres_nmuta', // your username
+    PASSWORD: 'passwordi', // your password
+    DB: 'mutual_funds', // your db name
+    dialect: 'postgres',
+    }
